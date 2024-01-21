db04b86dce67a07e83907943771f3402add58e7e
nathan muyoba <institous@yahoo.com>
2024-01-21 16:01:06 +0200

    postgresdb

--- TheScript/settings.py
+++ TheScript/settings.py
@@ -100,12 +100,12 @@ WSGI_APPLICATION = 'TheScript.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # Your password
 password = "TheScript.2404"
