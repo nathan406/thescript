70178924b33ecb80f77197703a043208db0deb1a
nathan muyoba <institous@yahoo.com>
2024-01-21 16:14:56 +0200

    static config

--- Base/urls.py
+++ Base/urls.py
@@ -5,14 +5,19 @@ from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
-    path('', views.index , name='home'),
-    path('article/<str:pk>', views.article , name='article'),
-    path('cyber/', views.cyber , name='cyber'),
-    path('programming/', views.programming , name='programming'),
-    path('tech/', views.tech , name='tech'),
+    path('', views.index, name='home'),
+    path('article/<str:pk>', views.article, name='article'),
+    path('cyber/', views.cyber, name='cyber'),
+    path('programming/', views.programming, name='programming'),
+    path('tech/', views.tech, name='tech'),
 
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
